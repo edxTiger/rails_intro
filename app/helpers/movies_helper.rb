4725b3c161c4d38fa13345474eea6dcaef11b5d6
Vitali Yanushchyk <vsomeg@gmail.com>
2014-11-25 04:05:06 -0300

    hw 2 part 1 complete

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,22 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+
+  def th_css_class(column)
+    column == sorting_column ? 'hilite' : nil
+  end
+
+  def sortable(title, column)
+    direction = (column == sorting_column && sort_direction == 'asc') ? 'desc' : 'asc'
+    link_to title, sort: column, direction: direction
+  end
+
+  def sort_direction
+    params[:direction] if %w[asc desc].include? params[:direction]
+  end
+
+  def sorting_column
+    params[:sort] if Movie.column_names.include? params[:sort]
+  end
+
 end
